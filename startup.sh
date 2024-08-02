842061ce5aed2431b45e5ef9452ff91f8252d673
ryan <ryan.hellerud@gmail.com>
2024-08-01 22:13:35 -0700

    updates

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 echo "Current directory: $(pwd)"
 echo "Contents of current directory:"
 ls -la
-echo "Contents of models directory:"
-ls -la models/
+echo "Contents of /home/site/wwwroot:"
+ls -la /home/site/wwwroot
+echo "Starting Gunicorn..."
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
